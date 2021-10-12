58489c84cf64ec8932415fea402b1ce805f709e2
shortnickname <selimasha@mail.ru>
2021-10-12 15:30:27 +0700

    разбиение на модули

---
+++ js/utilits.js
@@ -0,0 +1,12 @@
+// имя_функции(проверяемая_строка, максимальная_длина);
+// Результат: true, если строка проходит по длине, и false — если не проходит
+
+function verifyStrokeLength(stroke, maxLength) {
+  const strokeLength = stroke.length;
+  if (strokeLength <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
+verifyStrokeLength('some text', 10);
